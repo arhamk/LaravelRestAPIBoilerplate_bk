07d42393fa7939e0089b61e1d04f73d62badeff3
Arham Khalid <arham.khalid@intellexalsolutions.com>
2020-03-30 17:45:40 +0500

    Boilerplate changes

--- app/Api/V1/Controllers/ClientController.php
+++ app/Api/V1/Controllers/ClientController.php
@@ -10,45 +10,6 @@ use Tymon\JWTAuth\Exceptions\JWTException;
 
 class ClientController extends Controller
 {
-    /**
-     * @var
-     */
-    protected $user;
-
-    /**
-     * ClientController constructor.
-     */
-    // public function __construct()
-    // {
-    //     $this->user = JWTAuth::parseToken()->authenticate();
-    // }
-
-    /**
-     * @return mixed
-     */
-    public function index()
-    {
-       
-        $tasks = $this->user->tasks()->get(['title', 'description'])->all();
-        // print_r($tasks);
-        // exit;
-        return $tasks;
-    }
-
-    public function show($id)
-    {
-        $task = $this->user->tasks()->find($id);
-
-        if (!$task) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Sorry, task with id ' . $id . ' cannot be found.'
-            ], 400);
-        }
-
-        return $task;
-    }
-
         /**
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
@@ -86,60 +47,7 @@ class ClientController extends Controller
         }    
     }
 
-      /**
-     * @param $id
-    * @return \Illuminate\Http\JsonResponse
-    */
-   public function update(Request $request, $id)
-   {
-       $task = $this->user->tasks()->find($id);
-        // print_r($task);
-        // exit;
-       if (!$task) {
-           return response()->json([
-               'success' => false,
-               'message' => 'Sorry, task with id ' . $id . ' cannot be found.'
-           ], 400);
-       }
-   
-       $updated = $task->fill($request->all())->save();
-   
-       if ($updated) {
-           return response()->json([
-               'success' => true
-           ]);
-       } else {
-           return response()->json([
-               'success' => false,
-               'message' => 'Sorry, task could not be updated.'
-           ], 500);
-       }
-   }
 
-   /**
- * @param $id
- * @return \Illuminate\Http\JsonResponse
- */
-    public function destroy($id)
-    {
-        $task = $this->user->tasks()->find($id);
 
-        if (!$task) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Sorry, task with id ' . $id . ' cannot be found.'
-            ], 400);
-        }
 
-        if ($task->delete()) {
-            return response()->json([
-                'success' => true
-            ]);
-        } else {
-            return response()->json([
-                'success' => false,
-                'message' => 'Task could not be deleted.'
-            ], 500);
-        }
-    }
 }
